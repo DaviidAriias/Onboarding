74aadb3ede1ede1d41cf65e0b7a4a4d4b8c85f21
DaviidAriias <43676865+DaviidAriias@users.noreply.github.com>
2019-03-21 10:18:45 -0500

    s
    s

--- js/main.js
+++ js/main.js
@@ -38,6 +38,33 @@ function calcHeight() {
     }
 }
 
+    var deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (event) => {
+        // Prevent Chrome <= 67 from automatically showing the prompt
+        event.preventDefault();
+        // Stash the event so it can be triggered later.
+        installPromptEvent = event;
+        // Update the install UI to notify the user app can be installed
+        document.querySelector('#install-button').disabled = false;
+    });
+
+    btnInstall.addEventListener('click', () => {
+        // Update the install UI to remove the install button
+        document.querySelector('#install-button').disabled = true;
+        // Show the modal add to home screen dialog
+        installPromptEvent.prompt();
+        // Wait for the user to respond to the prompt
+        installPromptEvent.userChoice.then((choice) => {
+            if (choice.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            // Clear the saved prompt since it can't be used again
+            installPromptEvent = null;
+        });
+    });
+
 $(document).ready(function () {
     console.log('Window ready | JS');
 
@@ -120,34 +147,6 @@ $(window).on("load", function () {
             $('.play-btn').css('opacity', '0');
         });
     }
-
-
-    var deferredPrompt;
-    window.addEventListener('beforeinstallprompt', (event) => {
-        // Prevent Chrome <= 67 from automatically showing the prompt
-        event.preventDefault();
-        // Stash the event so it can be triggered later.
-        installPromptEvent = event;
-        // Update the install UI to notify the user app can be installed
-        document.querySelector('#install-button').disabled = false;
-      });
-
-      btnInstall.addEventListener('click', () => {
-        // Update the install UI to remove the install button
-        document.querySelector('#install-button').disabled = true;
-        // Show the modal add to home screen dialog
-        installPromptEvent.prompt();
-        // Wait for the user to respond to the prompt
-        installPromptEvent.userChoice.then((choice) => {
-          if (choice.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          // Clear the saved prompt since it can't be used again
-          installPromptEvent = null;
-        });
-      });
 
 });
 
